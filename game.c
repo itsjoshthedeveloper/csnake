5ab725f5fab945c1a2d3dc8ba06b4dd474ce5b96
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-06-11 17:24:09 -0400

    Create window, position, speed, snake structs

--- game.c
+++ game.c
@@ -3,6 +3,32 @@
 #include <unistd.h>
 #include <ncurses.h>
 
+typedef struct
+{
+    int fps;
+    int width;
+    int height;
+    int frame;
+} window;
+
+typedef struct
+{
+    int x;
+    int y;
+} position;
+
+typedef struct
+{
+    int x;
+    int y;
+} speed;
+
+typedef struct
+{
+    position headPosition;
+    speed headSpeed;
+} snake;
+
 int main()
 {
     initscr(); // Start curses mode
@@ -16,6 +42,9 @@ int main()
     printw("Welcome to the Snake Game\n");
     refresh();
     sleep(3);
+
+    window win = {30, 20, 10, 0};
+    snake s = {{9, 4}, {0, 0}};
 
     endwin();
     return 0;
