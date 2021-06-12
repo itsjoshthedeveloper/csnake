38eeaedc09c4c692aa955ca6ba163025cdae47c8
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-06-11 20:51:04 -0400

    Update game over

--- game.c
+++ game.c
@@ -130,16 +130,14 @@ int main()
             s.headPosition = (position){(s.headPosition.x + s.headSpeed.x), (s.headPosition.y + s.headSpeed.y)};
             if (s.headPosition.x < 0 || s.headPosition.x > (win.width - 1) || s.headPosition.y < 0 || s.headPosition.y > (win.height - 1))
             {
+                printw("Game over!\n");
+                refresh();
+                sleep(10);
                 break;
             }
         }
         win.frame++;
     }
-
-    clear();
-    printw("Game over!\n");
-    refresh();
-    sleep(3);
 
     endwin();
     return 0;
